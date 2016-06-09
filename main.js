bc65f8eac023ac704212b0d758e65868a10e8e1d
mollywhitnack <mollywhitnack@gmail.com>
2016-06-08 21:21:45 -0700

    user input names, random

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var toggle=0;
 var xs =[];
 var os = [];
 var gameOver = false;
+var cats = 0;
+var rand =0;
+var names = [];
 
 $(document).ready(init); 
 
@@ -10,21 +13,20 @@ function init(){
   console.log("ready");
   console.log("player o's turn");
 
- /* $('.col').click(function(){
-    if(!gameOver){
-      $('.col').click(turn);
-    }
-  });*/
-
   //player clicked on a box
   $('.col').click(turn);
   $('.restart').click(restartGame);
+  $('.startGame').click(startGame);
+
 
 }
 
 function turn(event){
-  if(!gameOver){
+  var empty = $(event.target).text();
+  console.log("empty: " ,empty);
+  if(!gameOver && empty === ''){
   console.log("Turn");
+  cats++;
   console.log(event.target.id);
   var place = parseInt(event.target.id);
   //var child = event.target;
@@ -36,11 +38,16 @@ function turn(event){
   if(toggle ==0){
     toggle =1;
     $('.player').text("Player O's turn");
+    $('.player').css('padding-top', '4px');
+    $('.player').css('padding-bottom', '14px');
     xs.push(place);
     if(checkWin(xs)){
       console.log("X is the WINNER");
-      //$('.col').text('');
-      $('.winner').text("X is the WINNER");
+      console.log('Name: ' , names[0]);
+      if(names[0] != undefined)
+        $('.winner').text( `Player X: ${names[0]} is the WINNER!`);
+      else
+        $('.winner').text( `Player X is the WINNER!`);
       gameOver = true;
     }
   }
@@ -50,14 +57,22 @@ function turn(event){
     os.push(place);
     if(checkWin(os)){
       console.log("O is the WINNER");
-      //$('.col').text('');
-      $('.winner').text("O is the WINNER");
+      console.log('Name: ' ,names[1]);
+      if(names[1]!= undefined)
+        $('.winner').text( `Player O: ${names[1]} is the WINNER!`);
+      else
+        $('.winner').text( `Player O is the WINNER!`);
       gameOver = true;
     }
   }
 
   console.log("xs:" , xs);
   console.log("os:", os);
+}
+
+if(cats == 9){
+  $('.winner').text("Cat's Game!")
+  gameOver =true;
 }
 }
 
@@ -92,26 +107,33 @@ function contains(isThis, insideThis){
   }
 
 
-  /*for(let i =0; i<isThis.length; i++){
-    console.log("check: ", isThis[i]);
-    if($.inArray(isThis[i], insideThis) == -1){
-      return false;
-      console.log("NOT in it");
-    }
-    else{
-      return true;
-      console.log("Is in it");
-    }
-  }*/
-
-
 function restartGame(){
   console.log("Restart Game");
   toggle=0;
   xs =[];
   os = [];
   places  = [];
+  names = [];
   $('.col').text('');
   $('.winner').text('');
+  $('.player').text('');
   gameOver = false;
+  cats = 0;
+  $('.playerx').val('');
+  $('.playero').val('');
+  $('.player').css('padding-top', '20px');
+  $('.player').css('padding-bottom', '20px');
+}
+
+function startGame(){
+  var playerx = $('.playerx').val();
+  var playero = $('.playero').val();
+  names = [playerx, playero];
+  rand = Math.floor(Math.random() * (2));
+  console.log("rand: " ,rand);
+  console.log("players: ", names);
+  $('.player').text(`${names[rand]} go first!`);
+  $('.player').css('padding-top', '4px');
+  $('.player').css('padding-bottom', '14px');
+  toggle = rand;
 }
